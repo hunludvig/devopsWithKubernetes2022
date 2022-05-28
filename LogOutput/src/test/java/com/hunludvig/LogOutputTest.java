1ec433f4250665c3974fbd971b94735a86d850e8
Adam Ludvig <adam.ludvig@in4mo.com>
2022-05-28 07:37:13 +0300

    Exercise 1.01: LogOutput

---
+++ LogOutput/src/test/java/com/hunludvig/LogOutputTest.java
@@ -0,0 +1,21 @@
+package com.hunludvig;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class LogOutputTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
