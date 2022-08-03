15298a469eaff6597633b3cca61159bca02c94e2
Adam Vento <adam.vento@in4mo.com>
2022-08-03 08:25:53 +0300

    Exercise 5.02: Add Service Mesh to Project

---
+++ init_local_cluster.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo "Create cluster"
+k3d cluster create --port 8089:80@loadbalancer --agents 2
+echo "Create flux secret"
+kubectl create namespace flux-system
+kubectl -n flux-system create secret generic sops-age --from-file=age.agekey=tmp/local-flux-cluster-key.age
+echo "Bootstrap Flux"
+flux bootstrap github --owner=hunludvig --repository=devopsWithKubernetes2022 --personal --path=clusters/local --branch=master
